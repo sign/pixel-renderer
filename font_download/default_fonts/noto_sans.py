e093d1afc7da52becd8775bf889b926f28851be6
rggdmonk <32217886+rggdmonk@users.noreply.github.com>
2025-10-19 14:52:25 +0100

    refactor(noto_sans): sort by name

--- font_download/default_fonts/noto_sans.py
+++ font_download/default_fonts/noto_sans.py
@@ -58,14 +58,6 @@ _COMMON_NOTO_SANS = [
         url="https://github.com/google/fonts/raw/601944d2ee97e76d4c456dc3d790db358c0c6ec5/ofl/notosansdevanagari/NotoSansDevanagari%5Bwdth,wght%5D.ttf",
     ),
     FontSource(
-        # ofl/notosanssc/NotoSansSC[wght].ttf
-        url="https://github.com/google/fonts/raw/601944d2ee97e76d4c456dc3d790db358c0c6ec5/ofl/notosanssc/NotoSansSC%5Bwght%5D.ttf",
-    ),
-    FontSource(
-        # ofl/notosanstc/NotoSansTC[wght].ttf
-        url="https://github.com/google/fonts/raw/601944d2ee97e76d4c456dc3d790db358c0c6ec5/ofl/notosanstc/NotoSansTC%5Bwght%5D.ttf",
-    ),
-    FontSource(
         # ofl/notosansjp/NotoSansJP[wght].ttf
         url="https://github.com/google/fonts/raw/601944d2ee97e76d4c456dc3d790db358c0c6ec5/ofl/notosansjp/NotoSansJP%5Bwght%5D.ttf",
     ),
@@ -73,28 +65,48 @@ _COMMON_NOTO_SANS = [
         # ofl/notosanskr/NotoSansKR[wght].ttf
         url="https://github.com/google/fonts/raw/601944d2ee97e76d4c456dc3d790db358c0c6ec5/ofl/notosanskr/NotoSansKR%5Bwght%5D.ttf",
     ),
+    FontSource(
+        # ofl/notosanssc/NotoSansSC[wght].ttf
+        url="https://github.com/google/fonts/raw/601944d2ee97e76d4c456dc3d790db358c0c6ec5/ofl/notosanssc/NotoSansSC%5Bwght%5D.ttf",
+    ),
+    FontSource(
+        # ofl/notosanstc/NotoSansTC[wght].ttf
+        url="https://github.com/google/fonts/raw/601944d2ee97e76d4c456dc3d790db358c0c6ec5/ofl/notosanstc/NotoSansTC%5Bwght%5D.ttf",
+    ),
 ]
 
 _LESS_COMMON_NOTO_SANS = [
     FontSource(
-        # ofl/notosanshebrew/NotoSansHebrew[wdth,wght].ttf
-        url="https://github.com/google/fonts/raw/601944d2ee97e76d4c456dc3d790db358c0c6ec5/ofl/notosanshebrew/NotoSansHebrew%5Bwdth,wght%5D.ttf"
+        # ofl/notosansarmenian/NotoSansArmenian[wdth,wght].ttf
+        url="https://github.com/google/fonts/raw/601944d2ee97e76d4c456dc3d790db358c0c6ec5/ofl/notosansarmenian/NotoSansArmenian%5Bwdth,wght%5D.ttf",
     ),
     FontSource(
-        # ofl/notosansthai/NotoSansThai[wdth,wght].ttf
-        url="https://github.com/google/fonts/raw/601944d2ee97e76d4c456dc3d790db358c0c6ec5/ofl/notosansthai/NotoSansThai%5Bwdth,wght%5D.ttf",
+        # ofl/notosansbengali/NotoSansBengali[wdth,wght].ttf
+        url="https://github.com/google/fonts/raw/601944d2ee97e76d4c456dc3d790db358c0c6ec5/ofl/notosansbengali/NotoSansBengali%5Bwdth,wght%5D.ttf",
     ),
     FontSource(
         # ofl/notosansethiopic/NotoSansEthiopic[wdth,wght].ttf
         url="https://github.com/google/fonts/raw/601944d2ee97e76d4c456dc3d790db358c0c6ec5/ofl/notosansethiopic/NotoSansEthiopic%5Bwdth,wght%5D.ttf",
     ),
     FontSource(
-        # ofl/notosansarmenian/NotoSansArmenian[wdth,wght].ttf
-        url="https://github.com/google/fonts/raw/601944d2ee97e76d4c456dc3d790db358c0c6ec5/ofl/notosansarmenian/NotoSansArmenian%5Bwdth,wght%5D.ttf",
-    ),
-    FontSource(
         # ofl/notosansgeorgian/NotoSansGeorgian[wdth,wght].ttf
         url="https://github.com/google/fonts/raw/601944d2ee97e76d4c456dc3d790db358c0c6ec5/ofl/notosansgeorgian/NotoSansGeorgian%5Bwdth,wght%5D.ttf",
+    ),
+    FontSource(
+        # ofl/notosansgujarati/NotoSansGujarati[wdth,wght].ttf
+        url="https://github.com/google/fonts/raw/601944d2ee97e76d4c456dc3d790db358c0c6ec5/ofl/notosansgujarati/NotoSansGujarati%5Bwdth,wght%5D.ttf",
+    ),
+    FontSource(
+        # ofl/notosansgurmukhi/NotoSansGurmukhi[wdth,wght].ttf
+        url="https://github.com/google/fonts/raw/601944d2ee97e76d4c456dc3d790db358c0c6ec5/ofl/notosansgurmukhi/NotoSansGurmukhi%5Bwdth,wght%5D.ttf",
+    ),
+    FontSource(
+        # ofl/notosanshebrew/NotoSansHebrew[wdth,wght].ttf
+        url="https://github.com/google/fonts/raw/601944d2ee97e76d4c456dc3d790db358c0c6ec5/ofl/notosanshebrew/NotoSansHebrew%5Bwdth,wght%5D.ttf"
+    ),
+    FontSource(
+        # ofl/notosanskannada/NotoSansKannada[wdth,wght].ttf
+        url="https://github.com/google/fonts/raw/601944d2ee97e76d4c456dc3d790db358c0c6ec5/ofl/notosanskannada/NotoSansKannada%5Bwdth,wght%5D.ttf",
     ),
     FontSource(
         # ofl/notosanskhmer/NotoSansKhmer[wdth,wght].ttf
@@ -105,24 +117,24 @@ _LESS_COMMON_NOTO_SANS = [
         url="https://github.com/google/fonts/raw/601944d2ee97e76d4c456dc3d790db358c0c6ec5/ofl/notosanslao/NotoSansLao%5Bwdth,wght%5D.ttf",
     ),
     FontSource(
+        # ofl/notosansmalayalam/NotoSansMalayalam[wdth,wght].ttf
+        url="https://github.com/google/fonts/raw/601944d2ee97e76d4c456dc3d790db358c0c6ec5/ofl/notosansmalayalam/NotoSansMalayalam%5Bwdth,wght%5D.ttf"
+    ),
+    FontSource(
+        # ofl/notosansmongolian/NotoSansMongolian-Regular.ttf
+        url="https://github.com/google/fonts/raw/601944d2ee97e76d4c456dc3d790db358c0c6ec5/ofl/notosansmongolian/NotoSansMongolian-Regular.ttf",
+    ),
+    FontSource(
         # ofl/notosansmyanmar/NotoSansMyanmar[wdth,wght].ttf
         url="https://github.com/google/fonts/raw/601944d2ee97e76d4c456dc3d790db358c0c6ec5/ofl/notosansmyanmar/NotoSansMyanmar%5Bwdth,wght%5D.ttf",
     ),
     FontSource(
+        # ofl/notosansoriya/NotoSansOriya[wdth,wght].ttf
+        url="https://github.com/google/fonts/raw/601944d2ee97e76d4c456dc3d790db358c0c6ec5/ofl/notosansoriya/NotoSansOriya%5Bwdth,wght%5D.ttf",
+    ),
+    FontSource(
         # ofl/notosanssinhala/NotoSansSinhala[wdth,wght].ttf
         url="https://github.com/google/fonts/raw/601944d2ee97e76d4c456dc3d790db358c0c6ec5/ofl/notosanssinhala/NotoSansSinhala%5Bwdth,wght%5D.ttf",
-    ),
-    FontSource(
-        # ofl/notosansbengali/NotoSansBengali[wdth,wght].ttf
-        url="https://github.com/google/fonts/raw/601944d2ee97e76d4c456dc3d790db358c0c6ec5/ofl/notosansbengali/NotoSansBengali%5Bwdth,wght%5D.ttf",
-    ),
-    FontSource(
-        # ofl/notosansgujarati/NotoSansGujarati[wdth,wght].ttf
-        url="https://github.com/google/fonts/raw/601944d2ee97e76d4c456dc3d790db358c0c6ec5/ofl/notosansgujarati/NotoSansGujarati%5Bwdth,wght%5D.ttf",
-    ),
-    FontSource(
-        # ofl/notosansgurmukhi/NotoSansGurmukhi[wdth,wght].ttf
-        url="https://github.com/google/fonts/raw/601944d2ee97e76d4c456dc3d790db358c0c6ec5/ofl/notosansgurmukhi/NotoSansGurmukhi%5Bwdth,wght%5D.ttf",
     ),
     FontSource(
         # ofl/notosanstamil/NotoSansTamil[wdth,wght].ttf
@@ -137,48 +149,16 @@ _LESS_COMMON_NOTO_SANS = [
         url="https://github.com/google/fonts/raw/601944d2ee97e76d4c456dc3d790db358c0c6ec5/ofl/notosanstelugu/NotoSansTelugu%5Bwdth,wght%5D.ttf",
     ),
     FontSource(
-        # ofl/notosanskannada/NotoSansKannada[wdth,wght].ttf
-        url="https://github.com/google/fonts/raw/601944d2ee97e76d4c456dc3d790db358c0c6ec5/ofl/notosanskannada/NotoSansKannada%5Bwdth,wght%5D.ttf",
-    ),
-    FontSource(
-        # ofl/notosansmalayalam/NotoSansMalayalam[wdth,wght].ttf
-        url="https://github.com/google/fonts/raw/601944d2ee97e76d4c456dc3d790db358c0c6ec5/ofl/notosansmalayalam/NotoSansMalayalam%5Bwdth,wght%5D.ttf"
-    ),
-    FontSource(
-        # ofl/notosansmongolian/NotoSansMongolian-Regular.ttf
-        url="https://github.com/google/fonts/raw/601944d2ee97e76d4c456dc3d790db358c0c6ec5/ofl/notosansmongolian/NotoSansMongolian-Regular.ttf",
-    ),
-    FontSource(
-        # ofl/notosansoriya/NotoSansOriya[wdth,wght].ttf
-        url="https://github.com/google/fonts/raw/601944d2ee97e76d4c456dc3d790db358c0c6ec5/ofl/notosansoriya/NotoSansOriya%5Bwdth,wght%5D.ttf",
+        # ofl/notosansthai/NotoSansThai[wdth,wght].ttf
+        url="https://github.com/google/fonts/raw/601944d2ee97e76d4c456dc3d790db358c0c6ec5/ofl/notosansthai/NotoSansThai%5Bwdth,wght%5D.ttf",
     ),
 ]
 
 
 _RARE_NOTO_SANS = [
     FontSource(
-        # ofl/notosanscanadianaboriginal/NotoSansCanadianAboriginal[wght].ttf
-        url="https://github.com/google/fonts/raw/601944d2ee97e76d4c456dc3d790db358c0c6ec5/ofl/notosanscanadianaboriginal/NotoSansCanadianAboriginal%5Bwght%5D.ttf",
-    ),
-    FontSource(
         # ofl/notosansadlamunjoined/NotoSansAdlamUnjoined[wght].ttf
         url="https://github.com/google/fonts/raw/601944d2ee97e76d4c456dc3d790db358c0c6ec5/ofl/notosansadlamunjoined/NotoSansAdlamUnjoined%5Bwght%5D.ttf"
-    ),
-    FontSource(
-        # ofl/notosanscherokee/NotoSansCherokee[wght].ttf
-        url="https://github.com/google/fonts/raw/601944d2ee97e76d4c456dc3d790db358c0c6ec5/ofl/notosanscherokee/NotoSansCherokee%5Bwght%5D.ttf",
-    ),
-    FontSource(
-        # ofl/notosansthaana/NotoSansThaana[wght].ttf
-        url="https://github.com/google/fonts/raw/601944d2ee97e76d4c456dc3d790db358c0c6ec5/ofl/notosansthaana/NotoSansThaana%5Bwght%5D.ttf"
-    ),
-    FontSource(
-        # ofl/notosansvai/NotoSansVai-Regular.ttf
-        url="https://github.com/google/fonts/raw/601944d2ee97e76d4c456dc3d790db358c0c6ec5/ofl/notosansvai/NotoSansVai-Regular.ttf"
-    ),
-    FontSource(
-        # ofl/notosansyi/NotoSansYi-Regular.ttf
-        url="https://github.com/google/fonts/raw/601944d2ee97e76d4c456dc3d790db358c0c6ec5/ofl/notosansyi/NotoSansYi-Regular.ttf"
     ),
     FontSource(
         # ofl/notosansavestan/NotoSansAvestan-Regular.ttf
@@ -205,27 +185,43 @@ _RARE_NOTO_SANS = [
         url="https://github.com/google/fonts/raw/601944d2ee97e76d4c456dc3d790db358c0c6ec5/ofl/notosansbuhid/NotoSansBuhid-Regular.ttf",
     ),
     FontSource(
+        # ofl/notosanscanadianaboriginal/NotoSansCanadianAboriginal[wght].ttf
+        url="https://github.com/google/fonts/raw/601944d2ee97e76d4c456dc3d790db358c0c6ec5/ofl/notosanscanadianaboriginal/NotoSansCanadianAboriginal%5Bwght%5D.ttf",
+    ),
+    FontSource(
         # ofl/notosanscarian/NotoSansCarian-Regular.ttf
         url="https://github.com/google/fonts/raw/601944d2ee97e76d4c456dc3d790db358c0c6ec5/ofl/notosanscarian/NotoSansCarian-Regular.ttf"
+    ),
+    FontSource(
+        # ofl/notosanscherokee/NotoSansCherokee[wght].ttf
+        url="https://github.com/google/fonts/raw/601944d2ee97e76d4c456dc3d790db358c0c6ec5/ofl/notosanscherokee/NotoSansCherokee%5Bwght%5D.ttf",
+    ),
+    FontSource(
+        # ofl/notosansthaana/NotoSansThaana[wght].ttf
+        url="https://github.com/google/fonts/raw/601944d2ee97e76d4c456dc3d790db358c0c6ec5/ofl/notosansthaana/NotoSansThaana%5Bwght%5D.ttf"
+    ),
+    FontSource(
+        # ofl/notosansvai/NotoSansVai-Regular.ttf
+        url="https://github.com/google/fonts/raw/601944d2ee97e76d4c456dc3d790db358c0c6ec5/ofl/notosansvai/NotoSansVai-Regular.ttf"
+    ),
+    FontSource(
+        # ofl/notosansyi/NotoSansYi-Regular.ttf
+        url="https://github.com/google/fonts/raw/601944d2ee97e76d4c456dc3d790db358c0c6ec5/ofl/notosansyi/NotoSansYi-Regular.ttf"
     ),
 ]
 
 _EXOTIC_NOTO_SANS = [
     FontSource(
-        # ofl/notosansegyptianhieroglyphs/NotoSansEgyptianHieroglyphs-Regular.ttf
-        url="https://github.com/google/fonts/raw/601944d2ee97e76d4c456dc3d790db358c0c6ec5/ofl/notosansegyptianhieroglyphs/NotoSansEgyptianHieroglyphs-Regular.ttf"
+        # ofl/notosansanatolianhieroglyphs/NotoSansAnatolianHieroglyphs-Regular.ttf
+        url="https://github.com/google/fonts/raw/601944d2ee97e76d4c456dc3d790db358c0c6ec5/ofl/notosansanatolianhieroglyphs/NotoSansAnatolianHieroglyphs-Regular.ttf"
     ),
     FontSource(
         # ofl/notosanscuneiform/NotoSansCuneiform-Regular.ttf
         url="https://github.com/google/fonts/raw/601944d2ee97e76d4c456dc3d790db358c0c6ec5/ofl/notosanscuneiform/NotoSansCuneiform-Regular.ttf",
     ),
     FontSource(
-        # ofl/notosansanatolianhieroglyphs/NotoSansAnatolianHieroglyphs-Regular.ttf
-        url="https://github.com/google/fonts/raw/601944d2ee97e76d4c456dc3d790db358c0c6ec5/ofl/notosansanatolianhieroglyphs/NotoSansAnatolianHieroglyphs-Regular.ttf"
-    ),
-    FontSource(
-        # ofl/notosansmeroitic/NotoSansMeroitic-Regular.ttf
-        url="https://github.com/google/fonts/raw/601944d2ee97e76d4c456dc3d790db358c0c6ec5/ofl/notosansmeroitic/NotoSansMeroitic-Regular.ttf",
+        # ofl/notosansegyptianhieroglyphs/NotoSansEgyptianHieroglyphs-Regular.ttf
+        url="https://github.com/google/fonts/raw/601944d2ee97e76d4c456dc3d790db358c0c6ec5/ofl/notosansegyptianhieroglyphs/NotoSansEgyptianHieroglyphs-Regular.ttf"
     ),
     FontSource(
         # ofl/notosanslineara/NotoSansLinearA-Regular.ttf
@@ -234,5 +230,9 @@ _EXOTIC_NOTO_SANS = [
     FontSource(
         # ofl/notosanslinearb/NotoSansLinearB-Regular.ttf
         url="https://github.com/google/fonts/raw/601944d2ee97e76d4c456dc3d790db358c0c6ec5/ofl/notosanslinearb/NotoSansLinearB-Regular.ttf",
+    ),
+    FontSource(
+        # ofl/notosansmeroitic/NotoSansMeroitic-Regular.ttf
+        url="https://github.com/google/fonts/raw/601944d2ee97e76d4c456dc3d790db358c0c6ec5/ofl/notosansmeroitic/NotoSansMeroitic-Regular.ttf",
     ),
 ]
